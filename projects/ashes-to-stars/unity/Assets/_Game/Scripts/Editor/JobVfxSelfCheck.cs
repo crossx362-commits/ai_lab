2761fea708c75f597e0832e7918c6e3734f49045
crossx362 <crossx362@gmail.com>
2026-08-16 11:30:55 +0900

    feat(game): 수호기사 방어막 이펙트 적용

--- projects/ashes-to-stars/unity/Assets/_Game/Scripts/Editor/JobVfxSelfCheck.cs
+++ projects/ashes-to-stars/unity/Assets/_Game/Scripts/Editor/JobVfxSelfCheck.cs
@@ -9,7 +9,7 @@ namespace AshesToStars
         {
             Debug.Assert(JobVfxSheets.IsReady, "[JobVfxSelfCheck] 직업 이펙트 시트를 읽지 못했다");
             Debug.Assert(JobVfxSheets.FrameCount == 8, "[JobVfxSelfCheck] 8프레임 시트가 아니다");
-            Debug.Assert(JobVfxSheets.SourceCount == 4, "[JobVfxSelfCheck] 사제 치유 시트가 등록되지 않았다");
+            Debug.Assert(JobVfxSheets.SourceCount == 5, "[JobVfxSelfCheck] 수호기사 방어막 시트가 등록되지 않았다");
             Debug.Log("[JobVfxSelfCheck] PASS");
         }
     }
